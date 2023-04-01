acadb3da9024b4d3afb716db44d5e414fcee1d90
Prince Roy <princeku.ray143@gmail.com>
2023-04-01 13:06:42 +0530

    implement joined a room

--- server/server.js
+++ server/server.js
@@ -1,26 +1,19 @@
 const io = require('socket.io')(3000, {
 	cors: {
-		origin: ['http://localhost:3001'],
+		origin: ['http://localhost:3001', '*'],
 	},
 });
 
-const users = {};
-
 io.on('connection', socket => {
 	console.log(socket.id);
-	socket.on('send-message', (message, room) => {
-		if (room === '') socket.broadcast.emit('receive-message', message);
-		else socket.to(room).emit('receive-message', message, room);
-	});
 
 	//Join room
 	socket.on('join-room', room => {
 		socket.join(room);
-	});
+		console.log(`You joined ${room}`);
 
-	socket.on('login', (user, userId) => {
-		console.log('users', users);
-		if (socket.connected) users[user] = userId;
-		socket.emit('users-list', users);
+		const roomSockets = io.sockets.adapter.rooms.get(room);
+		const users = roomSockets ? [...roomSockets.keys()] : [];
+		io.to(room).emit('updated-users', users);
 	});
 });
