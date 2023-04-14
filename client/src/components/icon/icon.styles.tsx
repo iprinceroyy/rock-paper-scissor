0e835d97f7cb79e7b4d669d7b5f10557e4e0555b
Prince Roy <princeku.ray143@gmail.com>
2023-04-14 11:12:29 +0530

    create background effect for winner in mobile view

--- client/src/components/icon/icon.styles.tsx
+++ client/src/components/icon/icon.styles.tsx
@@ -68,8 +68,8 @@ export const IconContainer = styled.div<BorderColorProps>`
 	background-image: linear-gradient(${({ title }) => selectColor(title)});
 	box-shadow: ${({ won, title }) =>
 		won
-			? `0 0 0 50px rgba(255, 255, 255, 0.03), 0 0 0 100px rgba(255, 255, 255, 0.01),
-		0 0 0 150px rgba(255, 255, 255, 0.01), 0 7px 0 -1px ${selectBoxShadow(title)}`
+			? `0 0 0 20px rgba(255, 255, 255, 0.03), 0 0 0 50px rgba(255, 255, 255, 0.02),
+		0 0 0 85px rgba(255, 255, 255, 0.015), 0 7px 0 -1px ${selectBoxShadow(title)}`
 			: `
 		0 7px 0 -1px ${selectBoxShadow(title)}`};
 	grid-area: ${({ title }) => title === 'rock' && `2 / 1 / span 2 / span 2`};
@@ -77,6 +77,13 @@ export const IconContainer = styled.div<BorderColorProps>`
 	@media (min-width: 720px) {
 		width: ${({ bigSize }) => bigSize && `10rem`};
 		height: ${({ bigSize }) => bigSize && `10rem`};
+
+		box-shadow: ${({ won, title }) =>
+			won
+				? `0 0 0 50px rgba(255, 255, 255, 0.03), 0 0 0 100px rgba(255, 255, 255, 0.01),
+		0 0 0 150px rgba(255, 255, 255, 0.01), 0 7px 0 -1px ${selectBoxShadow(title)}`
+				: `
+		0 7px 0 -1px ${selectBoxShadow(title)}`};
 	}
 `;
 
