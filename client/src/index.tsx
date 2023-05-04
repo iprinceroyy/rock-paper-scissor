cc210eaeaa515fb0002dce833ef81eb4620a7771
Prince Roy <princeku.ray143@gmail.com>
2023-05-05 00:43:35 +0530

    remove contexts

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -9,25 +9,12 @@ import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
 
-import { RulesProvider } from './contexts/rules.context';
-import { GameProvider } from './contexts/game.context';
-import { ScoreProvider } from './contexts/score.context';
-import { SocketProvider } from './contexts/socket.context';
-
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
 	<React.StrictMode>
 		<Provider store={store}>
 			<BrowserRouter>
-				<SocketProvider>
-					<RulesProvider>
-						<ScoreProvider>
-							<GameProvider>
-								<App />
-							</GameProvider>
-						</ScoreProvider>
-					</RulesProvider>
-				</SocketProvider>
+				<App />
 			</BrowserRouter>
 		</Provider>
 	</React.StrictMode>
