def6e1e5a6625d65daa67a585bb99098e3e94c2a
Prince Roy <princeku.ray143@gmail.com>
2023-05-12 01:04:49 +0530

    update cross-origin-access

--- server/server.js
+++ server/server.js
@@ -14,7 +14,6 @@ const server = http.createServer(app);
 const io = new Server(server, {
 	cors: {
 		origin: ['https://game-rock-paper-scissorss.netlify.app/', 'http://localhost:3000'],
-		methods: ['GET', 'POST'],
 		credentials: true,
 	},
 });
