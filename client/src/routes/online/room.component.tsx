2881cdf4bf9e85bbe60e0229e401787fe1aaac76
Prince Roy <princeku.ray143@gmail.com>
2023-05-13 12:46:22 +0530

    update socket url

--- client/src/routes/online/room.component.tsx
+++ client/src/routes/online/room.component.tsx
@@ -14,13 +14,7 @@ import { FormInput, RoomContainer } from './room.styles';
 export const socket = io(
 	process.env.NODE_ENV === 'production'
 		? `${process.env.REACT_APP_SERVER_URL}`
-		: 'http://localhost:4000',
-	{
-		withCredentials: true,
-		extraHeaders: {
-			'my-custom-header': 'abcd',
-		},
-	}
+		: 'http://localhost:4000'
 );
 
 const Room = (): JSX.Element => {
--- server/server.js
+++ server/server.js
@@ -13,77 +13,72 @@ const server = http.createServer(app);
 // Create instance of socket.io server
 const io = new Server(server, {
 	cors: {
-		origin: ['https://game-rock-paper-scissorss.netlify.app/', 'http://localhost:3000'],
-		credentials: true,
-		allowedHeaders: ['my-custom-header'],
-		methods: ['GET', 'POST'],
+		origin: '*',
 	},
 });
 
 let choices = { p1Choice: null, p2Choice: null };
 
-const init = () => {
-	io.on('connection', socket => {
-		// Listen join-room event
-		socket.on('join-room', room => {
-			// Connect the socket to the specified room
-			socket.join(room);
+io.on('connection', socket => {
+	// Listen join-room event
+	socket.on('join-room', room => {
+		// Connect the socket to the specified room
+		socket.join(room);
 
-			// Emit connected event to the client
-			io.to(room).emit('connected');
+		// Emit connected event to the client
+		io.to(room).emit('connected');
 
-			// Get users at a particular room
-			let roomSockets = io.sockets.adapter.rooms.get(room);
-			let users = roomSockets ? [...roomSockets.keys()] : [];
+		// Get users at a particular room
+		let roomSockets = io.sockets.adapter.rooms.get(room);
+		let users = roomSockets ? [...roomSockets.keys()] : [];
 
-			// If users add to 3, emit error message & remove out the 3rd person
-			users.length === 3 &&
-				io.to(socket.id).emit('full', `Sorry! two players are already in this room. game is on`) &&
-				socket.leave(room);
+		// If users add to 3, emit error message & remove out the 3rd person
+		users.length === 3 &&
+			io.to(socket.id).emit('full', `Sorry! two players are already in this room. game is on`) &&
+			socket.leave(room);
 
-			roomSockets = io.sockets.adapter.rooms.get(room);
-			users = roomSockets ? [...roomSockets.keys()] : [];
+		roomSockets = io.sockets.adapter.rooms.get(room);
+		users = roomSockets ? [...roomSockets.keys()] : [];
 
-			// Emit updated users to the client
-			io.to(room).emit('updated-users', users);
+		// Emit updated users to the client
+		io.to(room).emit('updated-users', users);
 
-			// Listen game-play event & emit the status to the opponent excluding the sender
-			socket.on('game-play', () => {
-				socket.broadcast.to(room).emit('status', 'Opponent picked! Your turn.');
-			});
+		// Listen game-play event & emit the status to the opponent excluding the sender
+		socket.on('game-play', () => {
+			socket.broadcast.to(room).emit('status', 'Opponent picked! Your turn.');
+		});
 
-			// Listen restart event & emit the restart-message to the opponent excluding the sender
-			socket.on('restart', () => {
-				socket.broadcast.to(room).emit('restart-message', 'Opponent wants to play again');
-			});
+		// Listen restart event & emit the restart-message to the opponent excluding the sender
+		socket.on('restart', () => {
+			socket.broadcast.to(room).emit('restart-message', 'Opponent wants to play again');
+		});
 
-			// Listen disconnect event & emit the disconnected event to the opponent excluding the sender
-			socket.on('disconnect', () => {
-				socket.broadcast.to(room).emit('disconnected', 'Opponent left the game');
-			});
+		// Listen disconnect event & emit the disconnected event to the opponent excluding the sender
+		socket.on('disconnect', () => {
+			socket.broadcast.to(room).emit('disconnected', 'Opponent left the game');
+		});
 
-			// Listen p1Choice event & choose the winner if p2Choice has picked his choice
-			socket.on('p1Choice', data => {
-				const { choice, room } = data;
-				choices['p1Choice'] = choice;
+		// Listen p1Choice event & choose the winner if p2Choice has picked his choice
+		socket.on('p1Choice', data => {
+			const { choice, room } = data;
+			choices['p1Choice'] = choice;
 
-				io.to(room).emit('p1Choice', { choice });
+			io.to(room).emit('p1Choice', { choice });
 
-				choices.p2Choice !== null && declareWinner(room);
-			});
+			choices.p2Choice !== null && declareWinner(room);
+		});
 
-			// Listen p2Choice event & choose the winner if p1Choice has picked his choice
-			socket.on('p2Choice', data => {
-				const { choice, room } = data;
-				choices['p2Choice'] = choice;
+		// Listen p2Choice event & choose the winner if p1Choice has picked his choice
+		socket.on('p2Choice', data => {
+			const { choice, room } = data;
+			choices['p2Choice'] = choice;
 
-				io.to(room).emit('p2Choice', { choice });
+			io.to(room).emit('p2Choice', { choice });
 
-				choices.p1Choice !== null && declareWinner(room);
-			});
+			choices.p1Choice !== null && declareWinner(room);
 		});
 	});
-};
+});
 
 /**
  * Declare winner among two players
@@ -124,8 +119,6 @@ const declareWinner = room => {
 	// Reset choices object
 	choices = { p1Choice: null, p2Choice: null };
 };
-
-init();
 
 server.listen(4000, () => {
 	console.log(`Server running on Port 4000`);
